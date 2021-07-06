1fa572d3452bf9b572e7a373cb3b057c77f3d6a5
houdini91 <mdstrauss91@gmail.com>
2021-07-06 09:26:20 +0300

    Test

---
+++ jenkins_k8s/scripts/kaniko_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOCKER_USERNAME=$1
+DOCKER_PASSWORD=$2
+AUTH=$(echo -n "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" | base64)
+cat << EOF > config.json
+{
+    "auths": {
+        " https://scribesecuriy.jfrog.io": {
+            "auth": "${AUTH}"
+        }
+    }
+}
+EOF
